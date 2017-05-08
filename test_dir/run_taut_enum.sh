480d74d4d830f6d63aba9b916663bd5e58a7e594
DavidACosgrove <davidacosgroveaz@gmail.com>
2017-05-08 09:55:08 +0100

    Updated README.

--- test_dir/run_taut_enum.sh
+++ test_dir/run_taut_enum.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# standardise 1,3,4-triazoles
+# standardise 1,2,4-triazoles
 ../src/exe_DEBUG/taut_enum -I triazoles.smi -O triazoles_std.smi --standardise-only
 
-# standardise 1,3,4-triazoles and do original enumeration
+# standardise 1,2,4-triazoles and do original enumeration
 ../src/exe_DEBUG/taut_enum -I triazoles.smi -O triazoles_orig_enum.smi --original-enumeration
 
-# standarise 1,3,4-triazoles and do extended enumeration
+# standarise 1,2,4-triazoles and do extended enumeration
 ../src/exe_DEBUG/taut_enum -I triazoles.smi -O triazoles_ext_enum.smi --extended-enumeration
 
-# standarise 1,3,4-triazoles and do both enumerations
+# standarise 1,2,4-triazoles and do both enumerations
 ../src/exe_DEBUG/taut_enum -I triazoles.smi -O .smi --original-enumeration | \
     ../src/exe_DEBUG/taut_enum -I .smi -O triazoles_full_enum.smi --extended-enumeration
